fdc9c45372df85b754bd9cc9a9fbf93e90d1d59f
LHTKing13 <massinissa.kour@gmail.com>
2026-09-07 00:40:34 +0100

    Solve foundations/10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+// divisable by 4 but not devisable by 100 except if also devisable by 400
+const leapYears = function (year) {
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
