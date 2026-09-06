11771582b24255ed8ebac5ae2f6b4cc02c4a684f
LHTKing13 <massinissa.kour@gmail.com>
2026-09-06 22:44:48 +0100

    Solve foundations/07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let reversedString = "";
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
+
+  // this is another way to write the logic
+  // return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
