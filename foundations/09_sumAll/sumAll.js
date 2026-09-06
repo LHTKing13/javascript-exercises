100d8e5f7f0ac9d7842b585cdb60e7784c95b33c
LHTKing13 <massinissa.kour@gmail.com>
2026-09-06 23:51:55 +0100

    Solve foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (numX, numY) {
+  if (!Number.isInteger(numX) || !Number.isInteger(numY) || numX < 0 || numY < 0) {
+    return "ERROR";
+  } else {
+    let smallNumber = numX > numY ? numY : numX;
+    let bigNumber = numY > numX ? numY : numX;
 
+    let sum = 0;
+    for (let i = smallNumber; i <= bigNumber; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
