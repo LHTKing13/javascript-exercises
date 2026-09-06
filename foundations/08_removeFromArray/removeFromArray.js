ca29170038587d441cfadd899403cd6879c5371a
LHTKing13 <massinissa.kour@gmail.com>
2026-09-06 23:16:15 +0100

    Solve foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // args is an array
+  return arr.filter((x) => !args.includes(x));
 };
+
+console.log(removeFromArray([1, 2, 3, 3, 4, 5], 3, 4, "tacos"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
