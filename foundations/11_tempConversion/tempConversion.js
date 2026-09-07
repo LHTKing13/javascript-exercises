fdea3e9cda1310f9e5293b70375055659a13487a
LHTKing13 <massinissa.kour@gmail.com>
2026-09-07 01:19:33 +0100

    Solve foundations/11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  // c = (f - 32) / 1.8
+  return parseFloat(((f - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  // f = (c * 1.8) + 32
+  return parseFloat((c * 1.8 + 32).toFixed(1));
 };
 
+console.log(convertToCelsius(-100));
+console.log(convertToFahrenheit(37.77778));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
